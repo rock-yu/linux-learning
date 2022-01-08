ab73df79e04c27a01d93e4bea2cd884c2eee970e
Rock YU <chenzhong_yu@hotmail.com>
2022-01-08 21:02:57 +1100

    Update find_large_files.sh
    Update "find" example with comments

--- find_large_files.sh
+++ find_large_files.sh
@@ -1,3 +1,10 @@
-SIZE=+50M
+# Use 'find' to search under current directory for files larger than 50M
+# 1) Tests: -type, -size
+# 2) User-defined Actions: -exec command {} ;   
+# where {} is a symbolic representation of the current pathname, and the semicolon is a required delimiter indicating the end of the command.
+# Because the brace and semicolon have special meaning to the shell, they must be auoted or escaped
+SIZE=+50M && find . -type f -size $SIZE -exec ls -lh '{}' 2>/dev/null ';'
 
-find ~ -type f -size $SIZE -exec ls -lh '{}' 2>/dev/null ';' 
+
+# Delete large files interactively with "-ok" action
+# SIZE=+500M && find . -type f -size $SIZE -ok rm '{}' ';'
